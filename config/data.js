396159ef9508bc235e25e7b2dd9f2c930e3e1bac
eonsinde <olasinde.eon@gmail.com>
2022-10-27 08:53:19 +0100

    created hardcored data for the application

---
+++ config/data.js
@@ -0,0 +1,151 @@
+export const coffeeCategory = [
+    {
+        id: '#0',
+        name: 'All'
+    },
+    {
+        id: '#1',
+        name: 'Cappuccino',
+    },
+    {
+        id: '#2',
+        name: 'Expresso',
+    },
+    {
+        id: '#3',
+        name: 'Latte',
+    },
+    {
+        id: '#4',
+        name: 'Flat White',
+    },
+    {
+        id: '#5',
+        name: 'Riddler ❔',
+    }
+]
+
+
+export const coffeeList = [
+    {
+        id: '#1',
+        name: 'Muchino',
+        category: 'Cappuccino',
+        price: '12.5',
+        rating: '4.5',
+        included: 'With Oat Milk',
+        image: "https://coffee.alexflipnote.dev/2kM8z-48yTQ_coffee.jpg",
+        description: "Though coffee is the basis for an endless array of beverages, its popularity is mainly attributed to its invigorating effect, which is produced by caffeine, an alkaloid present in coffee"
+    },
+    {
+        id: '#2',
+        name: 'Tasty Icy',
+        category: 'Expresso',
+        price: '12.0',
+        rating: '4.1',
+        included: 'With Oat Milk',
+        image: "https://coffee.alexflipnote.dev/1WSZ4NbDMZE_coffee.jpg",
+        description: "Though coffee is the basis for an endless array of beverages, its popularity is mainly attributed to its invigorating effect, which is produced by caffeine, an alkaloid present in coffee"
+    },
+    {
+        id: '#3',
+        name: 'Lips Slick',
+        category: 'Latte',
+        price: '30.5',
+        rating: '4.2',
+        included: 'With Oat Milk',
+        image: "https://coffee.alexflipnote.dev/azlQvfeRVDs_coffee.png",
+        description: "Though coffee is the basis for an endless array of beverages, its popularity is mainly attributed to its invigorating effect, which is produced by caffeine, an alkaloid present in coffee"
+    },
+    {
+        id: '#4',
+        name: 'Forsty Flow',
+        category: 'Flat White',
+        price: '28.3',
+        rating: '4.3',
+        included: 'With Lound Milk',
+        image: "https://coffee.alexflipnote.dev/U7H1m9pudp8_coffee.png",
+        description: "Though coffee is the basis for an endless array of beverages, its popularity is mainly attributed to its invigorating effect, which is produced by caffeine, an alkaloid present in coffee"
+    },
+    {
+        id: '#5',
+        name: 'Silk Milk',
+        category: 'Riddler ❔',
+        price: '15.0',
+        rating: '4.0',
+        included: 'With Oat Milk',
+        image: "https://coffee.alexflipnote.dev/HQdNrJrR8Z4_coffee.jpg",
+        description: "Though coffee is the basis for an endless array of beverages, its popularity is mainly attributed to its invigorating effect, which is produced by caffeine, an alkaloid present in coffee"
+    },
+    {
+        id: '#6',
+        name: 'Riddler Sips',
+        category: 'Expresso',
+        price: '48.56',
+        rating: '4.8',
+        included: 'With Pure Milk',
+        image: "https://coffee.alexflipnote.dev/_rA3v9eQioE_coffee.jpg",
+        description: "Though coffee is the basis for an endless array of beverages, its popularity is mainly attributed to its invigorating effect, which is produced by caffeine, an alkaloid present in coffee"
+    },
+    {
+        id: '#7',
+        name: 'Riddle Flavor',
+        category: 'Riddler ❔',
+        price: '40.76',
+        rating: '4.6',
+        included: 'With Oat Milk',
+        image: "https://coffee.alexflipnote.dev/ZuFdD1Kuwtg_coffee.jpg",
+        description: "Though coffee is the basis for an endless array of beverages, its popularity is mainly attributed to its invigorating effect, which is produced by caffeine, an alkaloid present in coffee"
+    },
+    {
+        id: '#8',
+        name: 'Pure Flavor',
+        category: 'Latte',
+        price: '40.56',
+        rating: '4.2',
+        included: 'With Oat Milk',
+        image: "https://coffee.alexflipnote.dev/7nV8RUWELIc_coffee.png",
+        description: "Though coffee is the basis for an endless array of beverages, its popularity is mainly attributed to its invigorating effect, which is produced by caffeine, an alkaloid present in coffee"
+    },
+    {
+        id: '#9',
+        name: 'Taste \' Feel',
+        category: 'Cappuccino',
+        price: '45.0',
+        rating: '4.6',
+        included: 'With Soya Milk',
+        image: "https://coffee.alexflipnote.dev/Dhdy4wxPLo8_coffee.jpg",
+        description: "Though coffee is the basis for an endless array of beverages, its popularity is mainly attributed to its invigorating effect, which is produced by caffeine, an alkaloid present in coffee"
+    },
+
+    {
+        id: '#10',
+        name: 'Tippy Taste',
+        category: 'Flat White',
+        price: '40.76',
+        rating: '4.6',
+        included: 'With Rare Ice',
+        image: "https://coffee.alexflipnote.dev/Z_o_zatJziQ_coffee.jpg",
+        description: "Though coffee is the basis for an endless array of beverages, its popularity is mainly attributed to its invigorating effect, which is produced by caffeine, an alkaloid present in coffee"
+    },
+    {
+        id: '#11',
+        name: 'Lick \'n Sip',
+        category: 'Latte',
+        price: '40.56',
+        rating: '4.2',
+        included: 'With Yound Oat',
+        image: "https://coffee.alexflipnote.dev/7nV8RUWELIc_coffee.png",
+        description: "Though coffee is the basis for an endless array of beverages, its popularity is mainly attributed to its invigorating effect, which is produced by caffeine, an alkaloid present in coffee"
+    },
+    {
+        id: '#12',
+        name: 'Sip \'n Sip',
+        category: 'Cappuccino',
+        price: '45.0',
+        rating: '4.6',
+        included: 'With Raw Supplements',
+        image: "https://coffee.alexflipnote.dev/SllKzLJLoMM_coffee.jpg",
+        description: "Though coffee is the basis for an endless array of beverages, its popularity is mainly attributed to its invigorating effect, which is produced by caffeine, an alkaloid present in coffee"
+    },
+]
